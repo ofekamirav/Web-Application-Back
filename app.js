9a3e134a24603c46c91a2c839df975034a4cda1c
ofekamirav <ofek.amirav@gmail.com>
2024-11-20 19:26:22 +0200

    server connection and body-parser setup are done

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ const express = require('express');
 const app = express();
 const dotenv=require('dotenv').config();
 const port =process.env.PORT;
+//mongoose decleration
+
+
+//**Import routes */
+
+//Setup Body-parser to analyzing URL
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+try{
+    /*mongoose connect
+//
+//
+//
+*/
+    //Server Connection
+    app.listen()(port,()=>console.log(`Server is running on port ${port}`));
+}
+catch(error){
+    console.log('Error when trying to start the server:',error);
+}
